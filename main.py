b617c334279aec5d9d9cdaf47529e1af46b65f5e
tyukavkin99 <tyukavkin99@gmail.com>
2024-11-11 14:26:33 +0000

    moved components

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from get_symbols import DataIngestion
-from get_stock_data import DataFrameStock
+from components.get_symbols import DataIngestion
+from components.get_stock_data import DataFrameStock
 import sys
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 from openpyxl.workbook import Workbook
 
 def get_dates():
+    ''' Returns the current date and date 2 years ago '''
     current = date.today()
     past_date = current - relativedelta(years=2)
     current, past_date = current.strftime("%Y-%m-%d"), past_date.strftime("%Y-%m-%d")
     return current, past_date
 
 def main():
+    '''Outputs requested stock data and number of stocks'''
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'}
     url = "https://finance.yahoo.com/u/yahoo-finance/watchlists/semiconductor-stocks/" 	
     symbol_list = DataIngestion(url, headers).get_data()
@@ -25,8 +27,8 @@ def main():
         .get_dataframe()\
         .reset_index()
     df['Date'] = df['Date'].dt.tz_localize(None)
-    df.to_excel("tickers.xlsx")
-    with open("number_of_stocks.txt", "w+") as f:
+    df.to_excel("data/tickers.xlsx")
+    with open("data/number_of_stocks.txt", "w+") as f:
         f.write(str(number_of_stocks))
 
 

569f2d8a5933848845d06cb537c7f0874552d745
tyukavkin99 <tyukavkin99@gmail.com>
2024-12-30 14:22:08 +0000

    fix url bug and refactor get_data_and_dates

--- src/get_data_and_dates.py
+++ src/get_data_and_dates.py
@@ -20,11 +20,15 @@ class GetDataDates:
     def get_data():
         '''Outputs requested stock data and number of stocks'''
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'}
-        url = "https://finance.yahoo.com/u/yahoo-finance/watchlists/semiconductor-stocks/" 	
+        url = "https://finance.yahoo.com/u/motif/watchlists/semiconductor-stocks/" 	
         symbol_list = DataIngestion.get_data(url, headers)
         current, past_date = GetDataDates.get_dates()
         stock_getter = DataFrameStock(symbol_list, current_date=current, past_date=past_date)
-        number_of_stocks = stock_getter.get_number_of_stocks()
+        stock_getter.set_number_of_stocks()
+        n_of_stocks = stock_getter.number_of_stocks
+        print("Stock data: ", stock_getter.stocks)        
+        print("Current date: ", stock_getter.current_date)
+        print("Past data: ", stock_getter.past_date)        
         df = stock_getter\
             .get_dataframe()\
             .reset_index()
@@ -32,5 +36,5 @@ class GetDataDates:
         df.to_excel("data/tickers.xlsx")
         print("New dataset was added")
         with open("data/number_of_stocks.txt", "w+") as f:
-            f.write(str(number_of_stocks))
+            f.write(str(n_of_stocks))
         print("Number of stocks was added")
--- src/get_stock_data.py
+++ src/get_stock_data.py
@@ -4,18 +4,18 @@ import yfinance as yf
 
 class DataFrameStock():
 	def __init__(self, stocks, current_date=None, past_date=None):
-		self._stocks = stocks
+		self.stocks = stocks
 		self._current_date = current_date
 		self._past_date = past_date
 		self._number_of_stocks = None
 	
 	@property	
 	def current_date(self):
-		print(self._current_date)
+		return self._current_date
 	
 	@property
 	def past_date(self):
-		print(self._past_date)
+		return self._past_date
 
 	@current_date.setter
 	def current_date(self, value):
@@ -26,12 +26,15 @@ class DataFrameStock():
 		self._past_date = value
 	
 	def get_dataframe(self):
-		df = yf.download(self._stocks, start=self._past_date, end=self._current_date, interval="1wk", auto_adjust=True)
+		df = yf.download(self.stocks, start=self.past_date, end=self.current_date, interval="1wk", auto_adjust=True)
 		return df
 
 	@property	
 	def number_of_stocks(self):
-		self._number_of_stocks = len(self._stocks)
 		return self._number_of_stocks
 
-
+	def set_number_of_stocks(self):
+		self._number_of_stocks = len(self.stocks)
+'''
+if __name__ == "__main__":
+	test1 = DataFrameStock(["NVDA"], 2024-12-30, 2022-12-30) '''
